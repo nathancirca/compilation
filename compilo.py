e3d920443de37a0a485d172c9970db06e602d8bd
Pierre Blachere <pierre.blachere@outlook.com>
2022-06-09 22:56:31 +0200

    pointeurs

--- compilo.py
+++ compilo.py
@@ -148,9 +148,9 @@ def type(expr):
 def type_assign(expr,lhs):
     if expr.data == "variable":
         return f"mov [{lhs}_type], [{expr.children[0].value}_type]"
-    elif expr.data == "nombre":
+    elif expr.data == "nombre" or expr.data=="adresse":
         return f"mov rcx,0\nmov [{lhs}_type], rcx"
-    elif expr.data == "pointer":
+    elif expr.data == "pointer" or expr.data=="malloc":
         return f"mov rcx,1\nmov [{lhs}_type], rcx"
     elif expr.data =="chaine":
         return f"mov rcx,2\nmov [{lhs}_type], rcx"
@@ -170,17 +170,6 @@ def type_assign(expr,lhs):
         raise Exception("Not implemented")
 
 
-
-def var_list(ast):
-    if isinstance(ast, lark.Token):
-        if ast.type == "IDENTIFIANT":
-            return {ast.value}
-        else :
-            return set()
-    s=set()
-    for c in ast.children:
-        s.update(var_list(c))
-    return s
 
 def compile_expr(expr):
     global index
@@ -261,36 +250,6 @@ def compile_expr(expr):
         raise Exception("Not implemented")
     
 
-def type_assign(expr,lhs):
-    if expr.data == "variable":
-        return f"mov rcx, [{expr.children[0].value}_type]\nmov [{lhs}_type], rcx"
-    elif expr.data == "nombre":
-        return f"mov rcx, 0\nmov [{lhs}_type], rcx"
-    elif expr.data == "pointer":
-        return f"mov rcx, 1\nmov [{lhs}_type], rcx"
-    elif expr.data =="chaine":
-        return f"mov rcx, 2\nmov [{lhs}_type], rcx"
-    elif expr.data == "binexpr":
-        t1 = type(expr.children[0])
-        t2 = type(expr.children[2])
-        if expr.children[1] == "+":
-            if (t1=="2" or t2=="2"):
-                return f"mov rcx, 2\nmov [{lhs}_type], rcx"
-            elif (t1=="1" or t2=="1"):
-                return f"mov rcx, 2\nmov [{lhs}_type], rcx"
-            else :
-                return f"mov rcx, 0\nmov [{lhs}_type], rcx"
-    elif expr.data == "parenexpr":
-        return type_assign(expr.children[0])
-    elif expr.data == "len":
-        return f"mov rcx, 0\nmov [{lhs}_type], rcx"
-    elif expr.data == "isequal":
-        return f"mov rcx, 0\nmov [{lhs}_type], rcx"
-    elif expr.data == "charat":
-        return f"mov rcx, 2\nmov [{lhs}_type], rcx"
-    else :
-        raise Exception("Not implemented")
-
 def compile_cmd(cmd):
     global index
     if cmd.data == "assignment":
@@ -365,11 +324,14 @@ def compile(prg):
         code = code.replace("VAR_DECL", var_decl)
         code = code.replace("RETURN",compile_expr(prg.children[2]))
         code = code.replace("BODY", compile_bloc(prg.children[1]))
-        code = code.replace("INIT", compile_vars(prg.children[0]))
+        code = code.replace("VAR_INIT", compile_vars(prg.children[0]))
+        g = open("demo.asm", "w")
+        g.write(code)
+        g.close()
         return code
 
-prg = grammaire.parse("""main(X) {X = "abcdef"; X.setcharAt( 3 ,"b"); return(X);}""")
-print(compile(prg))
+#prg = grammaire.parse("""main(X) {X = "abcdef"; X.setcharAt( 3 ,"b"); return(X);}""")
+#print(compile(prg))
 
 def gamma_expr(expr):
     if expr.data == "nombre":
@@ -400,3 +362,6 @@ def gamma_expr(expr):
     elif expr.data=="parenexpr":
         return gamma_expr(expr.children[0])
 
+prg=grammaire.parse(read_file("test.txt"))
+print(pp_prg(prg))
+compile(prg)
--- demo.asm
+++ demo.asm
@@ -1,9 +1,13 @@
-extern printf, atoi, strlen
+extern printf, atoi
 global main
 section .data
 fmt: db "%d", 10, 0
-X: dq 0
-X_type: dq 0
+var: dq 0
+var_type: dq 0
+a: dq 0
+a_type: dq 0
+q: dq 0
+q_type: dq 0
 
 section .text
 main:
@@ -12,194 +16,33 @@ main:
   push rdi
   push rsi
 
-X: dq 0
-X_type: dq 0
 
 mov rbx, [rbp+0x10]
 mov rdi,[rbx+8]
 call atoi
-mov [X],rax
+mov [a],rax
 
-mov rax,20
-mov [X_type], 0
-mov [X],rax
-mov rax, [X]
-push rax
-mov rax,12
-pop rbx
-mov rcx, [X_type]
-mov rdx,0
-                cmp rcx, rdx
-je eqadd5
-jne neqadd5
-eqadd5: cmp rcx, 0
-je intadd5
-cmp rcx, 1
-je pointadd5
-jne stradd5
-stradd5: 
-jmp fin5
-pointadd5:
-add rax,rbx
-jmp fin5
-intadd5:
- add rax, rbx
-jmp fin5
-                neqadd5: cmp rcx, 0
-je i15
-jne cp15
-i15: cmp rdx, 1
-je iaddp5
-jne iadds5
+mov edi,8
+extern malloc
+call malloc
+mov rcx,1
+mov [q_type], rcx
+mov [q],rax
+mov rax,2
+mov rcx,0
+mov [var_type], rcx
+mov [var],rax
 
-                cp15: cmp rcx, 1
-je p15
-jne cs15
-p15: cmp rdx, 0
-je iaddp5
-jne fin5
-
-                cs15: cmp rdx, 0
-je saddi5
-jne fin5
-
-                iaddp5: add rax,rbx
-jmp fin5
-
-                iadds5: lea rax, X
-mov rbx, X
-mov edx, 8
-mov rsi, rax
-mov edi, rbx
-mov ebx, 0
-call itoa
-mov edx, 1
-mov ebx, 2
-add edx, ebx
-movsx rdx, ebx
-sub rdx, 1
-mov len_concat, rdx
-movsx rdx, ebx
-mov r10, rdx
-mov r11d, 0
-movsx rdx, ebx
-mov r8, rdx
-mov e9d, 0
-cdqe
-mov edx, 16
-sub rdx, 1
-add rbx, rdx
-mov esi, 16
-mov edx, 0
-div rsi
-imul rbx, rbx, 16
-sub rsp, rbx
-mov rbx, rsp
-add rbx, 0
-mov rax, rbx
-mov i, 0
-jmp debut1
-debut4:
-mov eax, i
-cdqe
-cmp rbx, 6
-ja debut2
-mov ebx, i
-cdqe
-movzx ecx, [X+rbx]
-mov rdx, rax
-mov eax, i
-cdqe
-mov [rdx+rbx], cl
-jmp debut3
-fin4:
-debut3:
-add i, 1
-fin3:
-debut1:
-mov ebx, rax
-cmp eax, len_concat
-jl debut4
-mov rsp rsi
-fin1:
-debut2:
-mov ebx, rax
-cdqe
-movzx ecx, [12+rbx]
-mov rdx, rax
-mov eax, i
-cdqe
-mov [rdx+rbx], cl
-fin2:   
-                saddi5: lea rax, X
-mov rbx, X
-mov edx, 8
-mov rsi, rax
-mov edi, rbx
-mov ebx, 0
-call itoa
-mov edx, 1
-mov ebx, 2
-add edx, ebx
-movsx rdx, ebx
-sub rdx, 1
-mov len_concat, rdx
-movsx rdx, ebx
-mov r10, rdx
-mov r11d, 0
-movsx rdx, ebx
-mov r8, rdx
-mov e9d, 0
-cdqe
-mov edx, 16
-sub rdx, 1
-add rbx, rdx
-mov esi, 16
-mov edx, 0
-div rsi
-imul rbx, rbx, 16
-sub rsp, rbx
-mov rbx, rsp
-add rbx, 0
-mov rax, rbx
-mov i, 0
-jmp debut1
-debut4:
-mov eax, i
-cdqe
-cmp rbx, 6
-ja debut2
-mov ebx, i
-cdqe
-movzx ecx, [X+rbx]
-mov rdx, rax
-mov eax, i
-cdqe
-mov [rdx+rbx], cl
-jmp debut3
-fin4:
-debut3:
-add i, 1
-fin3:
-debut1:
-mov ebx, rax
-cmp eax, len_concat
-jl debut4
-mov rsp rsi
-fin1:
-debut2:
-mov ebx, rax
-cdqe
-movzx ecx, [12+rbx]
-mov rdx, rax
-mov eax, i
-cdqe
-mov [rdx+rbx], cl
-fin2:
-                fin5:
-mov [X_type], 0
-mov [X],rax
-mov rax, [X]
+push rbp
+mov rbp,rsp
+mov QWORD [rbp], var
+lea rax,[rbp]
+mov QWORD [rbp],rax
+pop rbp
+mov rcx,0
+mov [q_type], rcx
+mov [q],rax
+mov rax, [q]
 
   mov rdi, fmt
   mov rsi, rax
